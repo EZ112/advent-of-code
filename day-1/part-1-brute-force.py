6e4f0a7ca7c91d4703e5a0dfc92522f6e7def8d0
Izzi <izzidz@gmail.com>
2025-12-01 17:54:58 +0700

    add brute force approach

---
+++ day-1/part-1-brute-force.py
@@ -0,0 +1,31 @@
+import sys
+
+def main():
+    instruction = []
+    with open(sys.argv[1]) as r:
+        for text in r:
+            instruction.append(text.replace('\n',''))
+
+    curr = 50
+    zeros = 0
+    for rotate in instruction:
+        num = int(rotate[1:], base=10)
+        if(rotate[0]=='L'):
+            for _ in range(num):
+                curr-=1
+                if(curr<0):
+                    curr+=100
+        else:
+            for _ in range(num):
+                curr+=1
+                if(curr>100):
+                    curr-=100
+        curr%=100
+        if(curr == 0):
+            zeros+=1
+                
+        print(f'{rotate} -> {curr}: {zeros}')
+    print('zeros: ', zeros)
+
+if __name__ == "__main__":
+    main()
