d296f2e7fa1b14f9fcb8cf380e31505adb130369
Izzi <izzidz@gmail.com>
2025-12-01 14:05:09 +0700

    day 1

---
+++ day-1/main.py
@@ -0,0 +1,27 @@
+def main():
+    instruction = []
+    with open('input') as r:
+        for text in r:
+            instruction.append(text.replace('\n',''))
+
+    curr = 50
+    zeros = 0
+    for rotate in instruction:
+        if(rotate[0]=='L'):
+            left_rotate = int(rotate[1:], base=10)
+            curr-=left_rotate
+        else:
+            right_rotate = int(rotate[1:], base=10)
+            curr+=right_rotate
+        curr%=100
+
+        if(curr<0):
+            curr+=100;
+
+        if(curr==0):
+            zeros+=1
+        print(f'{rotate} -> {curr}')
+    print('zeros:',zeros)
+
+if __name__ == '__main__':
+    main()
---
+++ devenv.nix
@@ -0,0 +1,12 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  # https://devenv.sh/languages/
+  languages.python.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
