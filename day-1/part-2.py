006af81dc80e24b734a8398b24b29e062452c29d
Izzi <izzidz@gmail.com>
2025-12-01 16:46:59 +0700

    add day-1 part-2

---
+++ day-1/part-2.py
@@ -0,0 +1,35 @@
+def main():
+    instruction = []
+    with open('input') as r:
+        for text in r:
+            instruction.append(text.replace('\n',''))
+
+    curr = 50
+    zeros = 0
+    for rotate in instruction:
+        prev=curr
+        if(rotate[0]=='L'):
+            left_rotate = int(rotate[1:], base=10)
+            if(left_rotate>100):
+                zeros+=left_rotate//100
+                left_rotate%=100
+            
+            curr-=left_rotate
+        else:
+            right_rotate = int(rotate[1:], base=10)
+            if(right_rotate>100):
+                zeros+=right_rotate//100
+                right_rotate%=100
+
+            curr+=right_rotate
+
+        if(prev!=0 and (curr<0 or curr>100 or curr%100==0)):
+            zeros+=1
+        raw_curr = curr
+        curr%=100
+
+        print(f'curr zeros {rotate} -> {curr}({raw_curr}) : {zeros}')
+    print('zeros:',zeros)
+
+if __name__ == '__main__':
+    main()
